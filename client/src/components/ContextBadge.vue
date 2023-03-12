5f2eca9e9ceca67c09e87b44402fb3936b2af734
Theo Meinhardt <theo.meinhardt@batinfo.at>
2023-03-12 13:11:41 +0100

    context badge foundation

---
+++ client/src/components/ContextBadge.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container q-mt-lg q-mx-lg">
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.container {
+  border-radius: 15px;
+  border: 1.5px solid $accent;
+
+  background-color: $primary;
+  color: $accent;
+}
+</style>
---
+++ client/src/components/ContextBadgeList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ContextBadge>
+      <template #content>
+        <h1>Test</h1>
+      </template>
+    </ContextBadge>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ContextBadge from './ContextBadge.vue';
+</script>
