7f1e7748a6ecf15ef98519cc8821ef28f41ab0cf
Theo Meinhardt <theo.meinhardt@batinfo.at>
2023-03-13 20:34:04 +0100

    added manifest file & basic caching

---
+++ client/manifest.ts
@@ -0,0 +1,47 @@
+export default {
+  name: 'Drizzle Weather App (V2)',
+  short_name: 'Drizzle',
+  id: '/',
+  start_url: '/',
+  background_color: '#0f2f43',
+  theme_color: '#174664',
+  display: 'minimal-ui',
+  orientation: 'portrait',
+  lang: 'en',
+  icons: [
+    {
+      src: '/images/icons/android-chrome-192x192.png',
+      type: 'image/png',
+      sizes: '192x192',
+    },
+    {
+      src: '/images/icons/android-chrome-512x512.png',
+      type: 'image/png',
+      sizes: '512x512',
+    },
+    {
+      src: '/images/icons/apple-touch-icon.png',
+      type: 'image/png',
+      sizes: '180x180',
+    },
+    {
+      src: '/images/icons/favicon-16x16.png',
+      type: 'image/png',
+      sizes: '16x16',
+    },
+    {
+      src: '/images/icons/favicon-32x32.png',
+      type: 'image/png',
+      sizes: '32x32',
+    },
+    {
+      src: '/images/icons/mstile-150x150.png',
+      type: 'image/png',
+      sizes: '150x150',
+    },
+    {
+      src: '/images/icons/safari-pinned-tab.svg',
+      type: 'image/svg',
+    },
+  ],
+};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,10 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+import manifest from './manifest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,13 +16,35 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    sourcemap: true,
   },
   plugins: [
     vue({
       template: { transformAssetUrls },
     }),
     VitePWA({
-      includeAssets: ['**/*.{js,css,html,jpg,ico,png,ttf,woff2}'],
+      includeAssets: ['**/*.{js,css,xml,html,jpg,ico,png,ttf,woff2,webmanifest}'],
+      manifest,
+      strategies: 'generateSW',
+      workbox: {
+        mode: 'development',
+        runtimeCaching: [
+          {
+            urlPattern: /https:\/\/api.openweathermap.org\/data\/2.5\/(weather|forecast|air_pollution)\?lat=\d{2}\.\d*&lon=\d{2}\.\d*&appid=[a-zA-Z0-9]*(&units=metric)?/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'weatherData',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 1,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
     }),
     quasar({
       sassVariables: 'src/quasar-variables.sass',
